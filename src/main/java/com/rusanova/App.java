07d1923441cccf58e05adcbeb3b54648f6cc0048
thealeshka <alexfenin1234@gmail.com>
2019-10-01 16:06:59 +0300

    Initial commit

---
+++ src/main/java/com/rusanova/App.java
@@ -0,0 +1,12 @@
+package com.rusanova;
+
+import com.rusanova.getLine.GetLine;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println(GetLine.getLine("String.txt"));
+    }
+}
---
+++ src/main/java/com/rusanova/getLine/GetLine.java
@@ -0,0 +1,28 @@
+package com.rusanova.getLine;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+
+public final class GetLine {
+    public static String getLine(String url) {
+        StringBuilder result = new StringBuilder();
+        try {
+            Files.readAllLines(Path.of(url)).forEach(n -> result.append(n));
+        } catch (NoSuchFileException e) {
+            createFile(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result.toString();
+    }
+
+    private static void createFile(String url){
+        try {
+            Files.createFile(Path.of(url));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
